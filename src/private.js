fc8cdb948c413e41f1f0f8eada1c732504661f8a
tantion <hillcom@qq.com>
2014-04-11 10:07:07 +0800

    支持清晰度切换

--- src/private.js
+++ src/private.js
@@ -15315,6 +15315,10 @@ define('private/player', function (require, exports, module) {
         $alert = $('.video-alert'),
         yun = require('private/yun');
 
+    function postMessageToPlay (url) {
+        window.postMessage({action: 'play', url: url}, '*');
+    }
+
     function init () {
         yun.hasLogin()
         .done(function () {
@@ -15323,16 +15327,23 @@ define('private/player', function (require, exports, module) {
                 if (urls.length) {
                     var navs = $.map(urls, function (url, i) {
                         var m3u8 = url.vod_url,
-                            flv = url.vod_url_dt17;
+                            flv = url.vod_url_dt17,
+                            title = '流畅版';
+
+                        if (i === 1) {
+                            title = '高清版';
+                        }
+                        else if (i > 1) {
+                            title = '超清版';
+                        }
+
                         return '<li><a class="private-play-vod" href="/private/play.html?flv=' + encodeURIComponent(flv) + '&m3u8=' +
-                            encodeURIComponent(m3u8) + '" data-flv="' + flv + '" data-m3u8="' + m3u8 + '">播放地址' + i + '</a></li>';
+                            encodeURIComponent(m3u8) + '" data-flv="' + flv + '" data-m3u8="' + m3u8 + '">' + title + '</a></li>';
                     });
                     navs = navs.join('\n');
                     $nav.html(navs);
-                    window.postMessage({action: 'play', url: {
-                        flv: urls[0].vod_url_dt17,
-                        m3u8: urls[0].vod_url
-                    }}, '*');
+
+                    postMessageToPlay({flv: urls[0].vod_url_dt17, m3u8: urls[0].vod_url});
                 } else {
                     $alert.show().html('转码未完成，请稍候再试');
                 }
@@ -15343,6 +15354,17 @@ define('private/player', function (require, exports, module) {
         })
         .fail(function () {
             $alert.show().html('<a href="http://vod.xunlei.com" target="_blank">你需要先登录迅雷</a>');
+        });
+
+        $(document)
+        .on('click', '.private-play-vod', function (evt) {
+            evt.preventDefault();
+
+            var $link = $(this),
+                flv = $link.data('flv'),
+                m3u8 = $link.data('m3u8');
+
+            postMessageToPlay({flv: flv, m3u8: m3u8});
         });
     }
 
