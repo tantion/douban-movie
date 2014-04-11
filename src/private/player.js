fa0f983c9508bdc5d4bbc97b05c2316821825707
tantion <hillcom@qq.com>
2014-04-12 02:19:56 +0800

    refactor private/player

--- src/private/player.js
+++ src/private/player.js
@@ -9,52 +9,82 @@ define('private/player', function (require, exports, module) {
     }
 
     var $ = require('jquery'),
+        purl = require('purl'),
+        aelem = document.createElement('a'),
         $container = $('.container'),
         $content = $('.video-container'),
         $nav = $('.video-nav'),
         $alert = $('.video-alert'),
-        yun = require('private/yun');
+        yun = require('private/yun'),
+        alertify = require('alertify'),
+        params = purl(location.href).param(),
+        infohash = params.infohash || '',
+        url = params.url || '';
 
     function postMessageToPlay (url) {
         window.postMessage({action: 'play', url: url}, '*');
     }
 
+    function requestHash () {
+        var dfd = $.Deferred();
+
+        if (infohash) {
+            dfd.resovle(infohash);
+        } else {
+            if (url) {
+                yun.requestHash(url)
+                .done(function (hash) {
+                    infohash = hash;
+                    history.replaceState(null, null, location.pathname + '?infohash=' + hash);
+                })
+                .fail(function (msg) {
+                    dfd.reject(msg);
+                });
+            }
+        }
+
+        return dfd.promise();
+    }
+
     function init () {
-        yun.hasLogin()
-        .done(function () {
-            yun.requestUrl2()
+        requestHash()
+        .done(function (hash) {
+            yun.requestUrlByHash(hash)
             .done(function (urls) {
-                if (urls.length) {
-                    var navs = $.map(urls, function (url, i) {
-                        var m3u8 = url.vod_url,
-                            flv = url.vod_url_dt17,
-                            title = '流畅版';
-
-                        if (i === 1) {
-                            title = '高清版';
-                        }
-                        else if (i > 1) {
-                            title = '超清版';
-                        }
-
-                        return '<li><a class="private-play-vod" href="/private/play.html?flv=' + encodeURIComponent(flv) + '&m3u8=' +
-                            encodeURIComponent(m3u8) + '" data-flv="' + flv + '" data-m3u8="' + m3u8 + '">' + title +
-                            '</a><a href="' + flv + '">flv</a>' + '<a href="' + m3u8 + '">m3u8</a></li>';
-                    });
-                    navs = navs.join('\n');
-                    $nav.html(navs);
-
-                    postMessageToPlay({flv: urls[0].vod_url_dt17, m3u8: urls[0].vod_url});
-                } else {
-                    $alert.show().html('转码未完成，请稍候再试');
-                }
+                aelem.setAttribute('href', urls[0].vod_url);
+                url = 'http://' + aelem.host + '/*';
+                chrome.runtime.sendMessage({action: 'referer', data: {url: url}});
             })
-            .fail(function () {
-                $alert.show().html('加载播放地址失败');
+            .done(function (urls) {
+                var navs = $.map(urls, function (url, i) {
+                    var m3u8 = url.vod_url,
+                    flv = url.vod_url_dt17,
+                    title = '流畅版';
+
+                    if (i === 1) {
+                        title = '高清版';
+                    }
+                    else if (i > 1) {
+                        title = '超清版';
+                    }
+
+                    return '<li><a class="private-play-vod" href="/private/play.html?flv=' + encodeURIComponent(flv) + '&m3u8=' +
+                        encodeURIComponent(m3u8) + '" data-flv="' + flv + '" data-m3u8="' + m3u8 + '">' + title +
+                        '</a><a href="' + flv + '">flv</a>' + '<a href="' + m3u8 + '">m3u8</a></li>';
+                });
+                navs = navs.join('\n');
+                $nav.html(navs);
+
+                postMessageToPlay({flv: urls[0].vod_url_dt17, m3u8: urls[0].vod_url});
+            })
+            .fail(function (msg) {
+                alertify.error(msg || '未知错误');
             });
         })
-        .fail(function () {
-            $alert.show().html('<a href="http://vod.xunlei.com" target="_blank">你需要先登录迅雷</a>');
+        .fail(function (msg) {
+            if (msg) {
+                alertify.error(msg);
+            }
         });
 
         $(document)
