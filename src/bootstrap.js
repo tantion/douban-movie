ec87469410c7d61e123a2178d2af6c0edcc21098
tantion <hillcom@qq.com>
2014-05-20 15:55:44 +0800

    Added online play search

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -17262,9 +17262,24 @@ define('js/bt-iqiyi', function(require, exports, module) {
     var $ = require('jquery'),
         m = require('mustache'),
         ITEMS_CACHE = {},
-        timeout = 30 * 1000;
+        timeout = 30 * 1000,
+        sources = {
+            'www.iqiyi.com': '爱奇艺',
+            'www.letv.com': '乐视TV',
+            'www.56.com': '56视频',
+            'www.funshion.com': '风行视频',
+            'tv.sohu.com': '搜狐视频',
+            'v.qq.com': '腾讯视频',
+            'www.pps.tv': 'PPS',
+            'ipd.pps.tv': 'PPS',
+            'v.youku.com': '优酷',
+            'www.tudou.com': '土豆',
+            'vod.kankan.com': '迅雷看看',
+            'www.m1905.com': 'M1905',
+            'v.ku6.com': '酷六'
+        };
 
-    function searchTitle (title) {
+    function searchTitle (title, pubYear, istv) {
         var dfd = new $.Deferred(),
             items = [],
             url = 'http://so.iqiyi.com/so/q_#keyword#';
@@ -17292,38 +17307,69 @@ define('js/bt-iqiyi', function(require, exports, module) {
                 var $html = $($.parseHTML(html)),
                     $container = $html.find('.s_main-mx'),
                     $container2 = $html.find('.mod_sideright'),
-                    $items = null,
+                    $items = $container.find('[data-widget-searchlist-elem="item"]'),
                     items = [];
 
-                console.log($container, $container2);
 
-                $items.each(function () {
-                    var $item = $(this),
-                        $link = $item.find('.t a').eq(0),
-                        $desc = $item.find('.c-abstract').eq(0),
-                        title = $.trim($desc.text()),
-                        size = '',
-                        matches = title.match(/文件大小:([\w\.]+) /);
+                items = $.map($items, function (item) {
+                    var $item = $(item),
+                        $title = $item.find('.film_about h2'),
+                        $link = $title.find('a.orange'),
+                        title = $.trim($title.text()),
+                        year = $.trim($title.find('.gray12 a').text()).replace(/(\d+)年/, '$1'),
+                        type = title.match(/\[(\S+)\]/),
+                        href = $link.prop('href') || '',
+                        source = $link.length ? $link[0].hostname : '';
 
-                    if (!matches) {
-                        matches = title.match(/下载\(([\w\.]+)\)/);
-                    }
-                    title = $.trim($link.text()).replace(/^(.+)_免费高速下载.*$/, '$1');
-
-                    if (matches && matches.length > 1) {
-                        size = matches[1];
-                        if (size) {
-                            title += ' ' + size;
-                        }
+                    if (type && type.length) {
+                        type = type[1];
                     }
 
-                    if (title) {
-                        items.push({
-                            href: $link.attr('href'),
+                    if (!(/\d+/.test(year))) {
+                        year = '';
+                    }
+
+                    if (sources.hasOwnProperty(source)) {
+                        source = sources[source];
+                    }
+
+                    title = $link.attr('title');
+
+                    if (((istv && (type === '连续剧' || type === '纪录片')) || (!istv && type === '电影')) &&
+                        title && href && (!year || (year && parseInt(year, 10) === pubYear))) {
+                        return {
+                            source: source,
+                            type: type,
+                            year: year,
+                            href: href,
                             title: title
-                        });
+                        };
                     }
                 });
+
+                if (!istv) {
+                    $container2.find('> ul > li')
+                    .each(function () {
+                        var $item = $(this),
+                            $link = $item.find('a.piclist_img'),
+                            source = $link.length ? $link[0].hostname : '',
+                            title = $.trim($item.find('.piclist_title').text());
+
+                        if (sources.hasOwnProperty(source)) {
+                            source = sources[source];
+                        }
+
+                        if (title.indexOf('[电影]') > -1 && source) {
+                            items.push({
+                                source: source,
+                                type: '电影',
+                                year: '',
+                                href: $link.attr('href'),
+                                title: $link.find('img').attr('title') || title
+                            });
+                        }
+                    });
+                }
 
                 ITEMS_CACHE[url] = items;
                 if (items.length) {
@@ -17343,7 +17389,9 @@ define('js/bt-iqiyi', function(require, exports, module) {
     var tmpl = '{{#items}}' +
                '<dl class="movie-improve-bt-dl">' +
                   '<dt class="movie-improve-bt-title">' +
-                      '<a title="点击打开种子下载页面" target="_blank" href="{{href}}">{{title}}</a>' +
+                      '<a title="点击打开播放页面" target="_blank" href="{{href}}">' +
+                      '{{#source}}[{{source}}] {{/source}}{{title}}' +
+                      '</a>' +
                   '</dt>' +
                   '{{#files}}' +
                   '<dd class="movie-improve-bt-desc">{{&title}}</dd>' +
@@ -17374,7 +17422,7 @@ define('js/bt-iqiyi', function(require, exports, module) {
         if (title) {
             dfd.notify('正在加载搜索结果，请耐心等待...');
 
-            searchTitle(title)
+            searchTitle(title, subject.year, subject.istv)
             .done(function (items) {
                 dfd.resolve(renderItems(items), items.length);
             })
