e7793545d2189e54db8e07d3420fc1e92f9640f2
tantion <hillcom@qq.com>
2014-07-20 05:09:43 +0800

    Added gbk decode

--- src/background.js
+++ src/background.js
@@ -4,10 +4,49 @@
 // 为了修改 Referer 我容易吗
 //
 
+// 改变文件名
+chrome.webRequest.onHeadersReceived.addListener(function (details) {
+    "use strict";
+
+    var found = false;
+    var filename = details.url.match(/\/([^\/]+$)/);
+    var value = '';
+
+    if (filename && window.gbk) {
+        filename = filename[1];
+        try {
+            filename = window.gbk.decode(filename);
+        } catch (ex) {
+        }
+    }
+
+    filename =  filename || 'filename.rar';
+
+    value = 'attachment; filename="' + filename + '"';
+
+    for (var i = 0, len = details.responseHeaders.length; i < len; i += 1) {
+        if (details.responseHeaders[i].name === 'Content-Disposition') {
+            details.responseHeaders[i].value = value;
+            found = true;
+        }
+    }
+
+    if (!found) {
+        details.responseHeaders.push({
+            name: 'Content-Disposition',
+            value: value
+        });
+    }
+
+    return {responseHeaders: details.responseHeaders};
+},
+{urls: ["http://www.bitfish8.com/uploads/soft/*"]},
+["blocking", "responseHeaders"]);
+
 chrome.webRequest.onBeforeSendHeaders.addListener(function(details) {
     "use strict";
 
-    for (var i = 0; i < details.requestHeaders.length; i += 1) {
+    for (var i = 0, len = details.requestHeaders.length; i < len; i += 1) {
         if (details.requestHeaders[i].name === 'Referer') {
             details.requestHeaders[i].value = 'http://www.bttiantang.com/download.php';
             break;
@@ -21,7 +60,7 @@ chrome.webRequest.onBeforeSendHeaders.addListener(function(details) {
 chrome.webRequest.onBeforeSendHeaders.addListener(function(details) {
     "use strict";
 
-    for (var i = 0; i < details.requestHeaders.length; i += 1) {
+    for (var i = 0, len = details.requestHeaders.length; i < len; i += 1) {
         if (details.requestHeaders[i].name === 'Referer') {
             details.requestHeaders[i].value = details.url;
             break;
@@ -36,7 +75,7 @@ var urls = [];
 function refererHandle (details) {
     "use strict";
 
-    for (var i = 0; i < details.requestHeaders.length; i += 1) {
+    for (var i = 0, len = details.requestHeaders.length; i < len; i += 1) {
         if (details.requestHeaders[i].name === 'Referer') {
             details.requestHeaders[i].value = details.url;
             break;
